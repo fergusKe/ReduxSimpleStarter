694ee6f46013bdc29c787d293e0032d001204024
fergus <fergus.jingtai@gmail.com>
2017-01-27 11:25:34 +0800

    24. Building Lists with Map

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,9 +2,13 @@ import React from 'react';
 import VideoListItem from './video_list_item.js';
 
 const VideoList = (props) => {
+  const videoItem = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItem}
     </ul>
   )
 }
