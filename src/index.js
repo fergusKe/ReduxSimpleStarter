e8407848d5dba95418232c7169ad28cdc4819e2d
fergus <fergus.jingtai@gmail.com>
2017-01-27 11:18:15 +0800

    23. Props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list.js';
 
 const API_KEY = 'AIzaSyC2kEvpOkCYgxlkOYqf2C4Nzqo6hPHQyfY';
 
@@ -13,6 +13,7 @@ class App extends Component {
     this.state = { videos: [] };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      console.log(videos);
       this.setState({ videos });
     });
   }
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
